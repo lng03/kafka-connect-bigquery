2e33475caecb8c50500a264ae46d443ec1f0aba2
lng03 <jing.ng@sky.uk>
2022-12-12 02:21:36 +0000

    Revert "Reduce log levels"
    This reverts commit 2aa5647f71f369f0ebd50ee027284efb00a561df.

--- kcbq-connector/src/main/java/com/wepay/kafka/connect/bigquery/GCSToBQLoadRunnable.java
+++ kcbq-connector/src/main/java/com/wepay/kafka/connect/bigquery/GCSToBQLoadRunnable.java
@@ -303,21 +303,21 @@ public class GCSToBQLoadRunnable implements Runnable {
     int successfulDeletes = 0;
 
     if (numberOfBlobs == 0) {
-      logger.trace("No blobs to delete");
+      logger.info("No blobs to delete");
       return;
     }
 
-    logger.debug("Attempting to delete {} blobs", numberOfBlobs);
+    logger.info("Attempting to delete {} blobs", numberOfBlobs);
 
     try {
       // Archive the blobs
       List<BlobId> archiveResults = archiveBlobs(blobIdsToDelete);
       int archiveFailCount = archiveResults.size();
       if(archiveFailCount == 0){
-        logger.debug("Successfully archived all blobs");
+        logger.info("Successfully archived all blobs");
       } else {
         for(BlobId blobId: archiveResults){
-          logger.warn("Failed to archive {}", blobId.getName());
+          logger.info("Failed to archive {}", blobId.getName());
         }
       }
       // Issue a batch delete api call
