549212ccf85f5301a714cccb7991c45aa881eb87
tmohanbabusky <127131438+tmohanbabusky@users.noreply.github.com>
2023-06-27 16:16:48 +0100

    GOTTP-11698 Removed podname

--- kcbq-connector/src/main/java/com/wepay/kafka/connect/bigquery/BigQuerySinkTask.java
+++ kcbq-connector/src/main/java/com/wepay/kafka/connect/bigquery/BigQuerySinkTask.java
@@ -247,7 +247,7 @@ public class BigQuerySinkTask extends SinkTask {
 
     // create tableWriters
     Map<PartitionedTableId, TableWriterBuilder> tableWriterBuilders = new HashMap<>();
-    String bucketName = config.getString(BigQuerySinkConfig.GCS_BUCKET_NAME_CONFIG)+ "/" + podName;
+    String bucketName = config.getString(BigQuerySinkConfig.GCS_BUCKET_NAME_CONFIG);
     logger.debug("Value for bucketName {}",bucketName);
     for (SinkRecord record : records) {
       if (record.value() != null || config.getBoolean(BigQuerySinkConfig.DELETE_ENABLED_CONFIG)) {
@@ -539,8 +539,8 @@ public class BigQuerySinkTask extends SinkTask {
   private void startGCSToBQLoadTask() {
     logger.info("Attempting to start GCS Load Executor.");
     loadExecutor = Executors.newScheduledThreadPool(1);
-    String bucketName = config.getString(BigQuerySinkConfig.GCS_BUCKET_NAME_CONFIG)+ "/" + podName;
-
+    //String bucketName = config.getString(BigQuerySinkConfig.GCS_BUCKET_NAME_CONFIG)+ "/" + podName;
+    String bucketName = config.getString(BigQuerySinkConfig.GCS_BUCKET_NAME_CONFIG);
     logger.debug("Value for bucketName {}",bucketName);
     String directoryPrefix =  config.getString(BigQuerySinkConfig.GCS_FOLDER_NAME_CONFIG);
   //  String directoryPrefix =  config.getString(BigQuerySinkConfig.GCS_FOLDER_NAME_CONFIG);
